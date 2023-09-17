0be25ff4f4998c4cabc26364ad618d9430b1d533
ciceroferreirajsm <ciceronascimentu@gmail.com>
2023-09-16 22:52:08 -0300

    Adicionar arquivos de projeto.

---
+++ RConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+namespace RConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "CiceroTesteFila",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+                    //Caso a regra de negócio funcione executar um Ack para confirmar sucesso da mensagem
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    //Caso de erro na regra de negócio da um Nack para voltar a mensagem para fila e nao perder nos threads
+                    channel.BasicNack(ea.DeliveryTag, false, false);
+                }
+            };
+            channel.BasicConsume(queue: "CiceroTesteFila",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RProducer/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+namespace RProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            int count = 0;
+
+            //while (true)
+            {
+                channel.QueueDeclare(queue: "CiceroTesteFila",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+                string message = $"{count++} Mensagem de teste para fila do Nova!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: string.Empty,
+                                     routingKey: "CiceroTesteFila",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" [x] Sent {message}");
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+    }
+}
